d29216f8a5992a6d0b5c14ac1b6f6d020086c357
Sn0wFox <ruben.pericas@gmail.com>
2017-11-28 11:52:42 +0100

    Update World header (consistency + pre-split evolution_step())

--- src/World.h
+++ src/World.h
@@ -17,36 +17,35 @@
 class GridCell;
 
 class World {
- public:
+  public:
     World(int width, int height, uint32_t seed);
     ~World() { statfile_best_.close(); statfile_mean_.close(); }
-    void random_population();
-    void test_mutate();
 
-    int width_;
-    int height_;
+    int   width_;
+    int   height_;
+    int   time_;
+    int   death_;
+    int   new_mutant_;
+    float min_fitness_, max_fitness_;
 
-    uint32_t global_seed_;
-    std::mt19937 global_gen_;
-
-
-    GridCell** grid_cell_;
-
-    int time_;
-
-    float min_fitness_,max_fitness_;
-    int death_;
-    int new_mutant_;
+    uint32_t      global_seed_;
+    GridCell**    grid_cell_;
+    std::mt19937  global_gen_;
     std::ofstream statfile_best_;
     std::ofstream statfile_mean_;
 
+    void random_population();
+    void test_mutate();
     void run_evolution();
-
     void evolution_step();
-
     void init_environment();
-
     void stats();
+
+  protected:
+    void step_live_or_die();
+    void step_compute_fitness();
+    void step_reproduce();
+    void step_diffuse_degrade_proteins();
 };
 
 
