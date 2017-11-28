d3e2fc0e6738f7fbd654ad4c0fbef217edac0059
Sn0wFox <ruben.pericas@gmail.com>
2017-11-28 10:56:57 +0100

    Fix cuda kernel call macro import

--- src/CudaCaller.h
+++ src/CudaCaller.h
@@ -1,3 +1,6 @@
+#ifndef CUDA_CALLER_H
+#define CUDA_DALLER_H
+
 // Workaround to prevent Visual Studio from complaining
 // because of the weird cuda kernal call <<< >>>
 // See https://stackoverflow.com/a/27992604/6087025
@@ -9,4 +12,6 @@
   #define KERNEL_ARGS2(grid, block)
   #define KERNEL_ARGS3(grid, block, sh_mem)
   #define KERNEL_ARGS4(grid, block, sh_mem, stream)
-#end
+#endif
+
+#endif
