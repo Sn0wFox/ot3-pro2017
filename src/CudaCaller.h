07d9654373653b0457e701fe958b5ac7c3748a3e
Sn0wFox <ruben.pericas@gmail.com>
2017-11-28 10:53:03 +0100

    Add maccro to prevent VS from complaining about cuda kernel calls

---
+++ src/CudaCaller.h
@@ -0,0 +1,12 @@
+// Workaround to prevent Visual Studio from complaining
+// because of the weird cuda kernal call <<< >>>
+// See https://stackoverflow.com/a/27992604/6087025
+#ifdef __CUDACC__
+  #define KERNEL_ARGS2(grid, block) <<< grid, block >>>
+  #define KERNEL_ARGS3(grid, block, sh_mem) <<< grid, block, sh_mem >>>
+  #define KERNEL_ARGS4(grid, block, sh_mem, stream) <<< grid, block, sh_mem, stream >>>
+#else
+  #define KERNEL_ARGS2(grid, block)
+  #define KERNEL_ARGS3(grid, block, sh_mem)
+  #define KERNEL_ARGS4(grid, block, sh_mem, stream)
+#end
