4d9c95ff3a87b079a13e5cc86a8d4e32464381f5
Sn0wFox <ruben.pericas@gmail.com>
2018-01-20 12:14:23 +0100

    Fix lines endings

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,18 @@ then
 	cd ot3-pro2017
 	git checkout master
 	git pull
+	cp ../bench.py bench.py
 else
 	echo --------- Cloning project...
 	git clone https://github.com/Sn0wFox/ot3-pro2017.git
+	git fetch --All
 	cd ot3-pro2017
+	cp ../bench.py bench.py
 	echo --------- Building project...
 	cmake .
 	make -f "Makefile"
 fi
 
 echo --------- Running tests...
-echo TODO: run the tests
+python bench.py
 echo --------- All done.
