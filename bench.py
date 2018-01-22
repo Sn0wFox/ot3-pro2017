ee35b5ab8acaa3b90b69aff709095a07bcd39a5b
Sn0wFox <ruben.pericas@gmail.com>
2018-01-22 16:47:48 +0100

    Fix inconsistent indents...

--- bench.py
+++ bench.py
@@ -50,14 +50,14 @@ def main():
         build()
         do_test(test[1], test[2], test[3], test[0], report_name)
 
-	report_name = "bench_weak" + datetime.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d_%H_%M_%S') + ".csv"
+    report_name = "bench_weak" + datetime.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d_%H_%M_%S') + ".csv"
 
-	for test in tests_weak:
-		with open(report_name, 'a') as f:
-		    f.write(test[0] + ", ")
-		checkout(test[0])
-		build()
-		do_test(test[1], test[2], test[3], test[0], report_name)
+    for test in tests_weak:
+        with open(report_name, 'a') as f:
+            f.write(test[0] + ", ")
+        checkout(test[0])
+        build()
+        do_test(test[1], test[2], test[3], test[0], report_name)
 
 def do_test(steps, cells, cores, commit, report_name):
     time_taken = 0;
